a7dd878254289be9adc7ffe38e84ce90154e340d
dmitrav <dmitrav@inbox.ru>
2020-10-28 12:56:59 +0100

    v.0.0.16: classifier + saving the model

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,5 @@
 
-version = "v.0.0.15"
+version = "v.0.0.16"
 
 allowed_ppm_error = 5
 tic_normalization_scaling_factor = 10 ** 5
--- src/trials.py
+++ src/trials.py
@@ -8,5 +8,22 @@ from src.constants import data_path as path
 from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 
+from src.torch import cl
+import torch
+
 if __name__ == "__main__":
-    pass
+
+    path = path.replace('data', 'res')
+
+    _, X_test, _, y_test = cl.get_data(path + 'encodings.csv')
+
+    print('creating new model')
+    model = cl.Classifier(input_shape=100, n_batches=7)
+    print('loading state_dict()')
+    model.load_state_dict(torch.load(path + 'classifier.torch', map_location='cpu'))
+    model.eval()
+
+    predictions = model(torch.Tensor(X_test))
+    true_positives = (predictions.argmax(-1) == torch.LongTensor(y_test)).float().detach().numpy()
+    test_accuracy = true_positives.sum() / len(true_positives)
+    print('achieved accuracy:', test_accuracy)
