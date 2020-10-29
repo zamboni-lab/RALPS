bc70384736859fd80d39ad558a29881fb92b31bf
dmitrav <dmitrav@inbox.ru>
2020-10-29 12:10:11 +0100

    v.0.0.19: variation coefs for batch analysis added

--- src/batch_analysis.py
+++ src/batch_analysis.py
@@ -3,17 +3,17 @@ import numpy, pandas, seaborn
 from matplotlib import pyplot
 
 
-def plot_batch_cross_correlations(data, method_name, sample_types_of_interest=None, save_to='/Users/andreidm/ETH/projects/normalization/res/'):
+def get_samples_by_types_dict(samples_names, types_of_interest):
+    """ Create a dict like this: {'P1_FA_0001': ['P1_FA_0001_0306_0', ..., 'P1_FA_0001_0306_2'], ...}  """
 
-    samples_names = data.columns.values
-    if sample_types_of_interest is None:
+    if types_of_interest is None:
         # get unique types + filter out diluted samples
-        sample_types_of_interest = list(set(['_'.join(x.split('_')[:3]) for x in samples_names if x.split('_')[2] == '0001']))
+        types_of_interest = list(set(['_'.join(x.split('_')[:3]) for x in samples_names if x.split('_')[2] == '0001']))
 
     samples_by_types = {}
     for i, sample in enumerate(samples_names):
         # check which type this sample has
-        for type in sample_types_of_interest:
+        for type in types_of_interest:
             if type in sample and type not in samples_by_types:
                 # if new type, putt in the dict, create a list
                 samples_by_types[type] = [sample]
@@ -25,13 +25,20 @@ def plot_batch_cross_correlations(data, method_name, sample_types_of_interest=No
             else:
                 pass
 
+    return samples_by_types
+
+
+def plot_batch_cross_correlations(data, method_name, sample_types_of_interest=None, save_to='/Users/andreidm/ETH/projects/normalization/res/'):
+
+    samples_by_types = get_samples_by_types_dict(data.columns.values, sample_types_of_interest)
+
     if sample_types_of_interest is None:
         for i, type in enumerate(samples_by_types):
-            sample_df = data.loc[:, numpy.array(samples_by_types[type])]
-            sample_df.columns = [x[-6:] for x in sample_df.columns]
-            sample_df = sample_df.corr()
+            df = data.loc[:, numpy.array(samples_by_types[type])]
+            df.columns = [x[-6:] for x in df.columns]
+            df = df.corr()
 
-            seaborn.heatmap(sample_df)
+            seaborn.heatmap(df)
             pyplot.title('Cross correlations: {}: {}'.format(type, method_name))
             pyplot.tight_layout()
             pyplot.show()
@@ -40,12 +47,12 @@ def plot_batch_cross_correlations(data, method_name, sample_types_of_interest=No
         pyplot.figure(figsize=(12,8))
 
         for i, type in enumerate(samples_by_types):
-            sample_df = data.loc[:, numpy.array(samples_by_types[type])]
-            sample_df.columns = [x[-6:] for x in sample_df.columns]
-            sample_df = sample_df.corr()
+            df = data.loc[:, numpy.array(samples_by_types[type])]
+            df.columns = [x[-6:] for x in df.columns]
+            df = df.corr()
 
             ax = pyplot.subplot(2, 3, i+1)
-            seaborn.heatmap(sample_df)
+            seaborn.heatmap(df)
             ax.set_title(type)
 
         pyplot.suptitle('Cross correlations: {}'.format(method_name))
@@ -54,12 +61,31 @@ def plot_batch_cross_correlations(data, method_name, sample_types_of_interest=No
         pyplot.savefig(save_to + 'correlations_{}.pdf'.format(method_name.replace(' ', '_')))
 
 
+def compute_cv_for_samples_types(data, sample_types_of_interest=None):
+
+    samples_by_types = get_samples_by_types_dict(data.columns.values, sample_types_of_interest)
+
+    cv_dict = {}
+    for i, type in enumerate(samples_by_types):
+        values = data.loc[:, numpy.array(samples_by_types[type])].values
+        values = values.flatten()
+        cv_dict[type] = numpy.std(values) / numpy.mean(values)
+
+    return cv_dict
+
+
 if __name__ == '__main__':
 
     data = pandas.read_csv('/Users/andreidm/ETH/projects/normalization/data/filtered_data.csv')
     data = data.iloc[:, 3:]
 
-    plot_batch_cross_correlations(data, 'original samples',
-                                  sample_types_of_interest=['P1_FA_0001', 'P2_SF_0001',
-                                                            'P2_SFA_0001', 'P2_SRM_0001',
-                                                            'P2_SFA_0002', 'P1_FA_0008'])
+    # plot_batch_cross_correlations(data, 'original samples',
+    #                               sample_types_of_interest=['P1_FA_0001', 'P2_SF_0001',
+    #                                                         'P2_SFA_0001', 'P2_SRM_0001',
+    #                                                         'P2_SFA_0002', 'P1_FA_0008'])
+
+    res = compute_cv_for_samples_types(data, sample_types_of_interest=['P1_FA_0001', 'P2_SF_0001',
+                                                                       'P2_SFA_0001', 'P2_SRM_0001',
+                                                                       'P2_SFA_0002', 'P1_FA_0008'])
+
+    print(res)
--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,5 @@
 
-version = "v.0.0.18"
+version = "v.0.0.19"
 
 allowed_ppm_error = 5
 tic_normalization_scaling_factor = 10 ** 5
